36d905177d86ae430e7820dc921c89c5a028d724
1992JYP <37524453+1992JYP@users.noreply.github.com>
2020-01-02 21:03:46 +0900

    Add files via upload

--- src 2nd/Fe16f.java
+++ src 2nd/Fe16f.java
@@ -1,22 +1,26 @@
 import java.util.Random;
 
 public class Fe16f {
-	//로또뽑기 1~45
+	// 로또뽑기 1~45
 	public static void main(String[] args) {
-		
-		Random lotto=new Random();
-		
-		int[] lottonum=new int[6];
-		for (int i=0;i<6;i++) {//출력할때마다 랜덤
-			lottonum[i]=(lotto.nextInt(45)+1);
-			for (int j=0;j<i;j++) {
-				if(lottonum[j]==lottonum[i]) {
-					while(lottonum[j]==lottonum[i]) {
-						lottonum[i]=(lotto.nextInt(45)+1);
-					}
+
+		Random lotto = new Random();
+
+		int[] lottonum = new int[6];
+		for (int i = 0; i < 6; i++) {// 출력할때마다 랜덤
+			lottonum[i] = (lotto.nextInt(6) + 1);
+//			System.out.println(i + 1 + "번째 숫자 : " + lottonum[i]);
+			for (int j = 0; j < i; j++) {
+				if (lottonum[j] == lottonum[i]) {
+					lottonum[i] = (lotto.nextInt(6) + 1);
+					i--;
 				}
 			}
-			System.out.println(i+1+"번째 숫자 : "+(lotto.nextInt(45)+1));
+		}
+		
+		for(int i=0;i<6;i++) {
+			System.out.println(i + 1 + "번째 숫자 : " + lottonum[i]);
+
 		}
 	}
 }
